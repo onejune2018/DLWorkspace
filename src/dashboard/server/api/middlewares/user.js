14c4a1636470db94cc01aa90e03214ad67041aa9
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2019-11-26 16:13:13 +0800

    Dashboard backend: remove winbind dependency

--- src/dashboard/server/api/middlewares/user.js
+++ src/dashboard/server/api/middlewares/user.js
@@ -7,7 +7,6 @@ module.exports = (forceAuthenticated = true) => async (context, next) => {
   if ('email' in context.query && 'token' in context.query) {
     const { email, token } = context.query
     const user = context.state.user = User.fromToken(context, email, token)
-    await user.fillIdFromWinbind()
     await user.password
     await user.addGroupLink
     await user.WikiLink
--- src/dashboard/server/api/services/user.js
+++ src/dashboard/server/api/services/user.js
@@ -2,17 +2,13 @@ const { createHash } = require('crypto')
 
 const config = require('config')
 const jwt = require('jsonwebtoken')
-const fetch = require('node-fetch')
 
 const Service = require('./service')
-const Cluster = require('./cluster')
 
 const sign = config.get('sign')
-const winbind = config.has('winbind') ? config.get('winbind') : undefined
 const masterToken = config.get('masterToken')
 const addGroupLink = config.get('AddGroupLink')
 const WikiLink = config.get('WikiLink')
-const clusterIds = Object.keys(config.get('clusters'))
 
 class User extends Service {
   /**
@@ -88,37 +84,6 @@ class User extends Service {
       })
     }
     return this._token
-  }
-
-  async fillIdFromWinbind () {
-    if (winbind == null) {
-      this.context.log.warn('No winbind server, user will have no uid / gid, and will not sync user info to any cluster.')
-      return null
-    }
-
-    const params = new URLSearchParams({ userName: this.email })
-    const url = `${winbind}/domaininfo/GetUserId?${params}`
-    this.context.log.info({ url }, 'Winbind request')
-    const response = await fetch(url)
-    const data = await response.json()
-    this.context.log.info({ data }, 'Winbind response')
-
-    this.uid = data['uid']
-    this.gid = data['gid']
-    return data
-  }
-
-  async addUserToCluster (data) {
-    if (data == null) return
-
-    // Fix groups format
-    if (Array.isArray(data['groups'])) {
-      data['groups'] = JSON.stringify(data['groups'].map(e => String(e)))
-    }
-    const params = new URLSearchParams(Object.assign({ userName: this.email }, data))
-    for (const clusterId of clusterIds) {
-      new Cluster(this.context, clusterId).fetch('/AddUser?' + params)
-    }
   }
 
   /**
